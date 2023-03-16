d00beed049fcfd9428acd209a5412d5925a73198
admin <alperen87@hotmail.com>
2023-03-16 10:51:32 +0100

    Initial commit

---
+++ src/main/java/com/example/week3_rickandmortycharacter/Week3RickAndMortyCharacterApplication.java
@@ -0,0 +1,13 @@
+package com.example.week3_rickandmortycharacter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3RickAndMortyCharacterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week3RickAndMortyCharacterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/week3_rickandmortycharacter/Week3RickAndMortyCharacterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week3_rickandmortycharacter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3RickAndMortyCharacterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
