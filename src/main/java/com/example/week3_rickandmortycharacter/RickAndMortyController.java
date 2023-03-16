e8849f2f9a51d889f0d79d41799e2ac3052229a6
admin <alperen87@hotmail.com>
2023-03-16 11:38:37 +0100

    adopted Daniels Code

--- src/main/java/com/example/week3_rickandmortycharacter/RickAndMortyController.java
+++ src/main/java/com/example/week3_rickandmortycharacter/RickAndMortyController.java
@@ -1,4 +1,18 @@
 package com.example.week3_rickandmortycharacter;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/rickandmorty")
+@RequiredArgsConstructor
 public class RickAndMortyController {
+    private final RickAndMortyService service;
+
+    @GetMapping
+    RickAndMortyCharacter getRickAndMortyCharacter() {
+        return service.getRickAndMortyCharacter();
+    }
 }
--- src/main/java/com/example/week3_rickandmortycharacter/RickAndMortyService.java
+++ src/main/java/com/example/week3_rickandmortycharacter/RickAndMortyService.java
@@ -1,4 +1,25 @@
 package com.example.week3_rickandmortycharacter;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Objects;
+
+@Service
 public class RickAndMortyService {
+    public RickAndMortyCharacter getRickAndMortyCharacter() {}
+
+    private final WebClient webClient = WebClient.create("https://rickandmortyapi.com/api");
+
+    RickAndMortyCharacter getRickAndMortyCharacter(){
+        ResponseEntity<RickAndMortyCharacter> responseEntity = webClient
+                .get()
+                .uri("/characters")
+                .retrieve()
+                .toEntity(RickAndMortyCharacter.class)
+                .block()
+                ;
+        return Objects.requireNonNull(responseEntity).getBody();
+    }
 }
